638d45082365450affb586fa52eb9eaee2fe9edf
Ajay Chandrasekhar <ajichand2009@gmail.com>
2018-05-30 22:00:58 +0530

    Updates to Isolation Forest algorithm.

--- isolation_forest.py
+++ isolation_forest.py
@@ -1,150 +1,34 @@
 #!/usr/bin/python3
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 ########################################################################
 # CLASSES
 ########################################################################
 class Node:
-	def __init__(self,data):
+	def __init__(self,data,pivot_attr,pivot_val=None,depth=0):
 		self.left = None
 		self.right = None
 		self.data = data
+		self.pivot_attr = pivot_attr
+		self.pivot_val = pivot_val
+		self.depth = depth
+
+	def build_tree(self,height_limit=8):
+		while self.data.shape[1] > 1 or self.depth < height_limit:
+			left,right = partition(self.data,self.pivot_attr,self.pivot_val)
+			self.left = Node(left,self.pivot_attr,self.pivot_val,self.depth+1)
+			self.right = Node(right,self.pivot_attr,self.pivot_val,self.depth+1)
+			self.left.build_tree()
+			self.right.build_tree()
 	
-	def insert(self,data):
-		if depth <= max_depth:
-			if data < self.data:
-				if self.left is None:
-					self.left = Node(data)
-				else:
-					self.left.insert(data)
-			elif data > self.data:
-				if self.right is None:
-					self.right = Node(data)
-				else:
-					self.right.insert(data)
-			else:
-				self.data = data
-
-	def lookup(self,data,parent=None):
-		if data < self.data:
-			if self.left is None:
-				return None,None
-			return self.left.lookup(data,self)
-		elif data > self.data:
-			if self.right is None:
-				return None,None
-			return self.right.lookup(data,self)
-		else:
-			return self,parent
-	
-	def path_length(self,data,length=0):
-		if data < self.data:
-			if self.left is None:
-				return length
-			return self.left.path_length(data,length+1)
-		elif data > self.data:
-			if self.right is None:
-				return length
-			return self.right.path_length(data,length+1)
-		else:
-			return length
-
-	def children_count(self):
-		count = 0
-		if self.left:
-			count += 1
-		if self.right:
-			count += 1
-		return count
-
-	def delete(self,data):
-		node,parent = self.lookup(data)
-		if node is not None:
-			children_count = node.children_count()
-		if children_count == 0:
-			if parent:
-				if parent.left is node:
-					parent.left = None
-				else:
-					parent.right = None
-				del node
-			else:
-				self.data = None
-		elif children_count == 1:
-			if node.left:
-				n = node.left
-			else:
-				n = node.right
-			if parent:
-				if parent.left is node:
-					parent.left = n
-				else:
-					parent.right = n
-				del node
-			else:
-				self.left = n.left
-				self.right = n.right
-				self.data = n.data
-		else:
-			parent = node
-			successor = node.right
-			while successor.left:
-				parent = successor
-				successor = successor.left
-			node.data = successor.data
-			if parent.left == successor:
-				parent.left = successor.right
-			else:
-				parent.right = successor.right
-
 	def print_tree(self):
+		print(self.data)
 		if self.left:
 			self.left.print_tree()
-		print(self.data)
 		if self.right:
 			self.right.print_tree()
-
-	def compare_trees(self,node):
-		if node is None:
-			return False
-		if self.data != node.data:
-			return False
-		res = True
-		if self.left is None:
-			if node.left:
-				return False
-
-class Tree:
-	def __init__(self,data,max_depth,pivot_attr,pivot_val):
-		self.root_node = Node(data)
-		self.node_count = 1
-		self.depth = 0
-		self.max_depth = max_depth
-		# 'pivot_attr' is an index to a Numpy 1-D array.
-		self.pivot_attr = pivot_attr
-		# 'pivot_val' is the value of 'data[pivot_attr]' of the root node.
-		# This is the value around which the tree is built.
-		self.pivot_val = pivot_val
-
-	def add_node(self,data):
-		self.root_node.insert(data)
-	
-	def lookup_node(self,data):
-		self.root_node.lookup(data)
-	
-	def delete_node(self,data):
-		self.root_node.delete(data)
-
-	def path_length(self,data):
-		self.root_node.path_length(data)
-
-class Forest:
-	def __init__(self,data,num_trees):
-		self.tree_list = []
-		self.tree_list.append(Tree(data,8))
-		self.num_trees = num_trees
-
 ########################################################################
 # FUNCTIONS
 ########################################################################
@@ -163,19 +47,29 @@ def anomaly_score(x,tree,n):
 
 # Function to partition a matrix 'X' based on a pivot attribute
 # (row index of the matrix) and a pivot value
-def partition(X,pivot_attr,pivot_val):
+def partition(X,pivot_attr,pivot_val=None):
 	a = X[pivot_attr,:]
+	if pivot_val is None:
+		a_min = np.min(a)
+		a_max = np.max(a)
+		pivot_val = np.random.randint(a_min,a_max)
 	b = np.where(a<pivot_val)
-	c = np.where(a>pivot_val)
+	c = np.where(a>=pivot_val)
+#	d = np.where(a==pivot_val)
 	Y = X[:,b[0]]
 	Z = X[:,c[0]]
+#	W = X[:,d[0]]
 	return Y,Z
-	
+
 ########################################################################
 # MAIN
 ########################################################################
-X = np.random.randint(10,size=(3,10))+1
-Y,Z = partition(X,2,5)
-print(X)
-print(Y)
-print(Z)
+np.random.seed(1)
+X = np.random.randint(100,size=(2,10))+1
+
+X_train = X[:,0:7]
+X_test = X[:,7:10]
+
+t = Node(X_train,0)
+t.build_tree()
+t.print_tree()
